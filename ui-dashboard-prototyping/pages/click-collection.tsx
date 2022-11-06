0cdc1e7fa48f7c30608ebd667ca681eb01fe59bc
Martin Maroši <marvusm.mmi@gmail.com>
2022-11-06 15:35:00 +0100

    Allow mocking multiple users.

--- ui-dashboard-prototyping/pages/click-collection.tsx
+++ ui-dashboard-prototyping/pages/click-collection.tsx
@@ -1,8 +1,10 @@
 import { Button, Grid } from '@mui/material';
-import { useCustomEvent } from '../src/ReactClient';
+import { createRandomUser, User } from '../mock/user';
+import { useCustomEvent, useIdentify } from '../src/ReactClient';
 
 const ClickCollection = () => {
   const event = useCustomEvent()
+  const identify = useIdentify()
   return (
     <Grid container spacing={4}>
       <Grid item xs={4}>
@@ -10,6 +12,13 @@ const ClickCollection = () => {
           event('foo', {foo: 'bar'})
         }}>
           Click to collect foo event
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button onClick={() => {
+          identify<User>(createRandomUser())
+        }}>
+          Click to identify new user
         </Button>
       </Grid>
     </Grid>
