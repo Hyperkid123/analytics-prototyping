2c73b340bb799aa7c06035df5ecedec2f21b88d5
Martin Maroši <marvusm.mmi@gmail.com>
2022-11-06 14:38:25 +0100

    Fix modules resolve issue for next dev server.

--- ui-dashboard-prototyping/next.config.js
+++ ui-dashboard-prototyping/next.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      next: path.resolve(__dirname, '../node_modules/next')
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
