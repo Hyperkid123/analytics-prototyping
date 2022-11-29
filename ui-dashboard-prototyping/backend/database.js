2d9e6edc3c494a7bbcc51b0ced5fe39ca690fd89
Martin Maroši <marvusm.mmi@gmail.com>
2022-11-29 18:05:26 +0100

    Add new database functions

--- ui-dashboard-prototyping/backend/database.js
+++ ui-dashboard-prototyping/backend/database.js
@@ -37,4 +37,112 @@ const handleEventEmit = (event) => {
   return uuid
 }
 
+const getUserEvents = (email) => {
+  let data = {}
+  try {
+    data = fse.readJsonSync(DATABASE_FILE)    
+  } catch (error) {
+    data = {}
+  }
+
+  const events = data.events.filter(entry => entry?.user?.email === email)
+  return events;
+}
+
+const getJourneyEvents = (name) => {
+  // journey-events
+  let data = {}
+  try {
+    data = fse.readJsonSync(DATABASE_FILE)    
+  } catch (error) {
+    data = {}
+  }
+
+  const journeys = data.events.filter(entry => entry?.type.match(/^journey-/) && entry?.journeyName === name)
+  const groups = journeys.reduce((acc, curr) => {
+    if(!acc[curr.journeyId]) {
+      return {
+        ...acc,
+        [curr.journeyId]: [curr]
+      }
+    }
+
+    return {
+      ...acc,
+      [curr.journeyId]: [...acc[curr.journeyId], curr]
+    }
+  }, {})
+  return groups;
+}
+
+getEventsByTime = (startTimestamp, endTimestamp) => {
+  let data = {}
+  try {
+    data = fse.readJsonSync(DATABASE_FILE)    
+  } catch (error) {
+    data = {}
+  }
+
+  let startIndex = 0
+  if(startTimestamp) {
+    const firstEventIndex = data.events.findIndex(({ timestamp }) => timestamp > startTimestamp)
+    startIndex = firstEventIndex < 0 ? data.events.length :  firstEventIndex
+  }
+
+  let endIndex = data.events.length
+  if(endTimestamp) {
+    const lastEventIndex = data.events.findIndex(({ timestamp }) => {      
+      return !(timestamp < endTimestamp)
+    })
+  
+    endIndex = lastEventIndex < 0 ? data.events.length : lastEventIndex
+  }
+
+  return data.events.slice(startIndex, endIndex)
+}
+
+const getEventsByDate = (startTimestamp, endTimestamp) => {
+  return getEventsByTime(startTimestamp, endTimestamp)
+}
+
+const getActiveUsers = (startTimestamp, endTimestamp) => {
+  const events = getEventsByTime(startTimestamp, endTimestamp);
+  const activeUsers = events.reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr.user.id]: 1
+    }
+  } , {})
+  return activeUsers
+}
+
+const generateEmptyHeatMap = () => [...Array(7)].reduce((acc, _, index) => {
+  return {
+    ...acc,
+    [index + 1]: [...Array(24)].fill({count: 0, activeSessions: []})
+  }
+}, {})
+
+const getUserActivityHeatmap = (startTimestamp, endTimestamp) => {
+  const events = getEventsByTime(startTimestamp, endTimestamp);
+  const activeByDays = events.reduce((acc, curr) => {
+    const eventDate = new Date(curr.timestamp)
+    const eventDay = eventDate.getDay()
+    const eventHour = eventDate.getHours()
+    const eventSession = curr.sessionId
+    const sessionDatapoint = {...acc[eventDay][eventHour]}
+    if(!sessionDatapoint.activeSessions.includes(eventSession)) {
+      sessionDatapoint.activeSessions.push(eventSession)
+      sessionDatapoint.count += 1
+    }
+    return {...acc, [eventDay]: acc[eventDay].map((val, index) => index === eventHour ? sessionDatapoint : val)}
+  }, generateEmptyHeatMap())
+  return activeByDays
+}
+
+module.exports.getUserActivityHeatmap = getUserActivityHeatmap
+module.exports.getActiveUsers = getActiveUsers
+module.exports.getEventsByDate = getEventsByDate
+module.exports.getJourneyEvents = getJourneyEvents
+module.exports.getUserEvents = getUserEvents
 module.exports.handleEventEmit = handleEventEmit
--- ui-dashboard-prototyping/server.js
+++ ui-dashboard-prototyping/server.js
@@ -1,7 +1,7 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
-const { handleEventEmit } = require('./backend/database')
+const { handleEventEmit, getUserEvents, getJourneyEvents, getEventsByDate, getActiveUsers, getUserActivityHeatmap } = require('./backend/database')
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
@@ -19,6 +19,40 @@ app.prepare().then(() => {
       const { pathname, query } = parsedUrl
 
       if(pathname.match(/^\/api/)) {
+        if(pathname === '/api/event/users' && req.method === 'GET') {
+          req.setEncoding('utf-8');
+          const email = query.email
+          const events = getUserEvents(email)
+          res.end(JSON.stringify({events}))
+          return
+        }
+        if(pathname === '/api/event/active/users' && req.method === 'GET') {
+          const startTimestamp = query.start
+          const endTimestamp = query.end
+          const users = getActiveUsers(startTimestamp, endTimestamp)
+          res.end(JSON.stringify({users}))
+          return
+        }
+        if(pathname === '/api/event/users/heatmap' && req.method === 'GET') {
+          const startTimestamp = query.start
+          const endTimestamp = query.end
+          const heatmap = getUserActivityHeatmap(startTimestamp, endTimestamp)
+          res.end(JSON.stringify({heatmap}))
+          return
+        }
+        if(pathname === '/api/event/journeys' && req.method === 'GET') {
+          const journeyName = query.journey
+          const journeys = getJourneyEvents(journeyName)
+          res.end(JSON.stringify({journeys}))
+          return
+        }
+        if(pathname === '/api/event/activity' && req.method === 'GET') {
+          const startTimestamp = query.start
+          const endTimestamp = query.end
+          const events = getEventsByDate(Number(startTimestamp), Number(endTimestamp))
+          res.end(JSON.stringify({events}))
+          return
+        }
         if(pathname === '/api/event' && req.method === 'POST') {
           // console.log('req.method', req)
           req.setEncoding('utf-8')
