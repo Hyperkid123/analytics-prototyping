c1323e06f17df73b0a5ea7a306f2fb9f7c95c5f4
Dr. Brantley <dr.brantley@redhat.com>
2022-12-08 14:24:45 -0500

    Refactor models for bytes and jsonb

--- backend/models/users.go
+++ backend/models/users.go
@@ -2,12 +2,11 @@ package models
 
 import (
 	"github.com/google/uuid"
-	"github.com/jackc/pgtype"
 )
 
 // Users
 type User struct {
 	BaseModel
 	UserID uuid.UUID    `json:"userID"`               // Store the internal UUID for that user
-	Data   pgtype.JSONB `json:"-" gorm:"column:data"` // Store additional metadata for users
+	Data   []byte	    `json:"-" gorm:"type:jsonb;column:data"` // Store additional metadata for users
 }
