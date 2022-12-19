23b135170010c54695301b778735124b20134c96
Martin Maroši <marvusm.mmi@gmail.com>
2022-12-19 09:51:50 +0100

    Add proper dragg support for pendo overlay builder.

--- pendo-like-overlay-prototype/src/lib/GuidesPropertiesEditor.svelte
+++ pendo-like-overlay-prototype/src/lib/GuidesPropertiesEditor.svelte
@@ -1,45 +1,31 @@
 <script>
   import { onDestroy, onMount } from "svelte";
   import { selectedElement } from "../stores/selectedElement";
+  import interact from 'interactjs'
 
-  let containerPositionTop = 0
-  let containerPositionLeft = 0
-
-  var isMouseDown = false;
-  addEventListener("mousedown", ()=>isMouseDown = true);
-  addEventListener("mouseup", ()=>isMouseDown = false);
-
-  function drag(ev) {
-    const rect = ev.target.getBoundingClientRect();
-    containerPositionTop = ev.pageY
-    containerPositionLeft = ev.pageX
-  }
-
-  function dragEnd(ev) {
-    containerPositionTop = ev.pageY
-    containerPositionLeft = ev.pageX
-  }
-
-  function dragStart(ev) {        
-    const crt = this.cloneNode(true);
-    // crt.style.display = "none"; hide the drag ghost
-    ev.dataTransfer.setDragImage(crt, 0, 0);
-    ev.dataTransfer.dropEffect = "move";
-    ev.dataTransfer
-    .setData("text", ev.target.getAttribute('id'));
-  }
+  let overlayElement
+  const position = { x: 0, y: 0 }
 
   onMount(() => {
-    if($selectedElement) {
-      // update initial position to prevent target element blocking
-      const rect = $selectedElement.getBoundingClientRect()
-      containerPositionTop = rect.top + rect.height + 8
+    if($selectedElement && overlayElement) {
+      console.log(selectedElement, overlayElement)
+      interact(overlayElement).draggable({
+        listeners: {
+          move (event) {
+            position.x += event.dx
+            position.y += event.dy
+  
+            event.target.style.transform =
+              `translate(${position.x}px, ${position.y}px)`
+          },
+        }
+      })
     }
   })
 </script>
 
 
-<div id="guide-overlay-main-container" style="left: {containerPositionLeft}px; top: {containerPositionTop}px" class="overlay" on:dragstart={dragStart} on:dragend={dragEnd} on:drag={drag} draggable="true">
+<div bind:this={overlayElement} id="guide-overlay-main-container" class="overlay">
   <div draggable="false">
     <h1>There will be dragons</h1>
   </div>
@@ -49,5 +35,8 @@
   .overlay {
     background-color: azure;
     position: fixed;
+    width: 300px;
+    height: 300px;
+    top: 0
   }
 </style>
